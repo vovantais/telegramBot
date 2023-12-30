4531862cd7f05dd13cf44f1b7070903955df3add
uladzimir_hiadzeika@senla.eu <uladzimir_hiadzeika@senla.eu>
2023-12-30 13:58:10 +0100

    init: start project

---
+++ config.js
@@ -0,0 +1,5 @@
+export const config = {
+  tokenTelegram: "YOUR_TOKEN",
+  catUrl: "https://api.thecatapi.com/v1/images/search",
+  weatherUrl: `http://api.weatherapi.com/v1/current.json?key={YOUR_API_KEY}&q=`,
+};
